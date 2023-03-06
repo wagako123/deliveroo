88278cc751b462ebe5c837af2cd54a35541719dc
unknown <brianwagako@gmail.com>
2023-03-06 16:24:16 +0300

    fixedcategorycard

--- components/Categories.js
+++ components/Categories.js
@@ -8,14 +8,12 @@ const Categories = () => {
     <ScrollView contentContainerStyle={{paddingHorizontal:15, paddingTop:10,}} 
     horizontal showsHorizontalScrollIndicator={false}>
         {/*Category Card */}
-        <CategoryCard 
-        imgUrl='https://links.papareact.com/wru'
-         title="Testing 1"/>
-        <CategoryCard imgUrl='https://links.papareact.com/gn7' title="Testing 2"/>
-        <CategoryCard imgUrl='https://links.papareact.com/wru' title="Testing 3"/>
-        <CategoryCard imgUrl='https://links.papareact.com/wru' title="Testing 3"/>
-        <CategoryCard imgUrl='https://links.papareact.com/wru' title="Testing 3"/>
-        <CategoryCard imgUrl='https://links.papareact.com/wru' title="Testing 3"/>
+        <CategoryCard imgUrl='https://links.papareact.com/wru' title='Testing 1'/>
+        <CategoryCard imgUrl='https://links.papareact.com/wru' title='Testing 2'/>
+        <CategoryCard imgUrl='https://links.papareact.com/wru' title='Testing 3'/>
+        <CategoryCard imgUrl='https://links.papareact.com/wru' title='Testing 4'/>
+        <CategoryCard imgUrl='https://links.papareact.com/wru' title='Testing 5'/>
+        <CategoryCard imgUrl='https://links.papareact.com/wru' title='Testing 6'/>
 
 
         </ScrollView>
--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -1,13 +1,12 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
-import { Image, Text, TouchableOpacity, View } from 'react-native'
 import tailwind from 'tailwind-rn'
 
-const CategoryCard = (imgUrl, title,) => {
+const CategoryCard = ({imgUrl,title,}) => {
   return (
-    <TouchableOpacity style={tailwind('relative mr-2')}>
-        <Image source={{uri:'https://links.papareact.com/wru'}}
-        style={tailwind('h-20 w-20 rounded')}/>
-      <Text style={tailwind('absolute bottom-1 left-1 text-white font-bold')}>title</Text>
+    <TouchableOpacity>
+      <Image source={{uri:imgUrl}} style={tailwind('h-20 w-20 rounded')}/>
+      <Text style={tailwind('absolute bottom-1 left-1 text-white font-bold')}>{title}</Text>
     </TouchableOpacity>
   )
 }
